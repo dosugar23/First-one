38d67ce2071d24726cb0a319b5427a51f8f4185e
Bao Nguyen <55596457+dosugar23@users.noreply.github.com>
2019-11-22 10:06:58 +0700

    Add files via upload

---
+++ dumb.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Dumb
+{
+	public static void main(String[]args)
+	{
+
+	double a,b,c,d;
+	Scanner sc = new Scanner(System.in);
+	
+
+	System.out.println("What is your first number? ");
+	a = sc.nextDouble();
+	System.out.println("What is your second number? ");
+	b = sc.nextDouble();
+	System.out.println("What is your third number? ");
+	c = sc.nextDouble();
+	System.out.println("What is your fourth number? ");
+	d = sc.nextDouble();
+	System.out.println(a+"+"+b+"+"+c+"+"+d+"/2 = 12 " );
+
+
+
+
+	}
+}
